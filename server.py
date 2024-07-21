fc06819457e262a64d14cf7315e45e96558038cf
serverbro <mantaco.mc.0123456789@gmail.com>
2024-07-21 02:13:26 +0000

    Mundo guardado 2024-07-21

--- server.py
+++ server.py
@@ -54,7 +54,7 @@ def download_latest_release(download_path='.'):
 			time.sleep(1.5)
 		pathto = os.path.join(download_path, version)
 		with open(pathto, 'wb') as archivo:x
-			archivo.write(requests.get(url).content)
+		archivo.write(requests.get(url).content)
 		return version
 flnm=download_latest_release()
 if flnm.split(".")[-1] == "msp":
